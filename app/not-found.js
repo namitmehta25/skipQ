b9d5cd2679a6250246666d79c4b8c1de649b5742
namitmehta25 <namitmehta25106@gmail.com>
2025-04-09 22:43:57 +0530

    fix

--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ export default function Custom404() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
       <h1 className="text-4xl font-bold">404 - Food Not Found</h1>
-      <p className="text-lg">Sorry, we couldn't find the page you're looking for.</p>
+      <p className="text-lg">Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>
       <Link href="/">
         <span className="text-indigo-400 hover:text-indigo-300 mt-4 cursor-pointer transition-all duration-300 hover:underline">
              Go back home
